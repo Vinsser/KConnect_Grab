394e03b0f730e5e68fff44de38d3e8ad4d45f121
VinayGenM <vinay.karajagi@genmills.com>
2021-09-20 20:30:58 +0530

    adding args

--- conngrab.py
+++ conngrab.py
@@ -3,32 +3,49 @@ import json
 import sys
 
 args = sys.argv[1:]
-
 seed_url = str(args[0])
 
-#connectors = []
+if (len(args) < 2) : step_arg = 'default' 
+else: step_arg = args[1]
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-seed_response = requests.get(seed_url, verify=False)
-
-connectors = seed_response.json()
-
-for conn in connectors:
+# method to grab a single available connector config
+def one_status(conn):
     status_url = seed_url + '/' + conn + '/status'
     status_response = requests.get(status_url, verify=False)
     status_dict = status_response.json()
-    #jprint(status_reponse.json())
-    print('##############################')
     print('connector_name: ' + status_dict['name'], '|','connector_state: ' + status_dict['connector']['state'])
-    for T in status_dict['tasks']:
-        print( T['id'], T['state'])
-        if T['state'] == 'FAILED': print(seed_url + '/' + conn + '/tasks/' + str(T['id']) + '/restart')
-    #print( [(T['id'], T['state']) for T in status_dict['tasks']] )
-    print('\n')
-    #print(status_dict)
+    jprint(status_dict['tasks'])
+    #if status_dict['state'] == 'FAILED': print(seed_url + '/' + conn + '/tasks/' + str(status_dict['id']) + '/restart')
+
+# method to grab all available connector configs
+def all_status():
+    for conn in connectors:
+        status_url = seed_url + '/' + conn + '/status'
+        status_response = requests.get(status_url, verify=False)
+        status_dict = status_response.json()
+        #jprint(status_reponse.json())
+        print('##############################')
+        print('connector_name: ' + status_dict['name'], '|','connector_state: ' + status_dict['connector']['state'])
+        for T in status_dict['tasks']:
+            print( T['id'], T['state'])
+            if T['state'] == 'FAILED': print(seed_url + '/' + conn + '/tasks/' + str(T['id']) + '/restart')
+        #print( [(T['id'], T['state']) for T in status_dict['tasks']] )
+        print('\n')
+        #print(status_dict)
+
+seed_response = requests.get(seed_url, verify=False)
+connectors = seed_response.json()
+
+if step_arg == 'default': jprint(connectors) 
+elif step_arg == 'all': all_status()
+else: one_status(step_arg)
+    
+
+
 
 
